0f748e97cae584fed7186bf1c9b73d901c78c628
ljs981026 <ljs981026@naver.com>
2024-05-01 03:57:17 +0900

    상세페이지(기본정보, 스텟 요소 추가)

---
+++ src/components/BaseStat.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from 'react'
+
+const BaseStat = ({valueStat, nameStat, type}) => {
+  console.log(valueStat, nameStat, type)
+  const bg = `bg-${type}`;
+
+  const ref = useRef(null);
+  
+  useEffect(() => {
+    const setValueStat = ref.current;
+    const calc = valueStat * (100/255)
+    setValueStat.style.width = calc + '%';
+  }, [])  
+
+  return (
+    <tr className='w-full text-white'>
+      <td className='sm:px-5'>
+        {nameStat}
+      </td>
+      <td className='px-2 sm:px-3'>
+        {valueStat}
+      </td>
+      <td>
+        <div className={`flex items-start h-2 min-w-[10rem] overflow-hidden rounded bg-gray-600`}>
+          <div className={`h-3 ${bg}`} ref={ref}></div>
+        </div>
+      </td>
+      <td className='px-2 sm:px-5'>255</td>
+    </tr>
+  )
+}
+
+export default BaseStat
